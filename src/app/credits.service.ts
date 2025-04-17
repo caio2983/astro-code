259fa206eca76f275c99f4fade554f770c91036f
caio2983 <caioluna2002@gmail.com>
2025-04-16 23:35:36 -0300

    feat: create credits service

---
+++ src/app/credits.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CreditsService {
+  private http = inject(HttpClient);
+
+  getUsers(): Observable<any[]> {
+    console.log('get user service');
+    return this.http.get<any[]>('http://localhost:3000/users');
+  }
+
+  creditsTransaction(credits: string | number, id: number | string) {
+    return this.http.get(`http://localhost:3000/credit/${credits}/${id}`);
+  }
+
+  constructor() {}
+}
