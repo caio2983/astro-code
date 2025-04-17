ee44b865026dc433f8bea0d87f8fb0be30b927aa
caio2983 <caioluna2002@gmail.com>
2025-04-16 23:36:19 -0300

    feat: update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreditsService } from './credits.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AsyncPipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'astro-code';
+
+  users$!: Observable<any[]>;
+
+  constructor(private credits: CreditsService) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.users$ = this.credits.getUsers();
+  }
+
+  creditsTransaction(credits: number | string, id: number | string) {
+    this.credits.creditsTransaction(credits, id).subscribe((response) => {
+      this.getUsers();
+    });
+
+    console.log('transaction test');
+  }
 }
