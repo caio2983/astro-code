7254e9f786cf5126a0177fd115da8430fa76dcd5
caio2983 <caioluna2002@gmail.com>
2025-04-17 23:47:39 -0300

    update misc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,42 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CreditsService } from './credits.service';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { AsyncPipe, NgFor } from '@angular/common';
+import { UserlistComponent } from './userlist/userlist.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgFor, AsyncPipe],
+  imports: [UserlistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'astro-code';
-
-  users$!: Observable<any[]>;
-  transactions!: any[];
-
-  constructor(private credits: CreditsService) {}
-
-  ngOnInit(): void {
-    this.getUsers();
-  }
-
-  getTransactions() {
-    this.credits.getTransactions().subscribe((data) => {
-      this.transactions = data;
-      console.log('dados recebidos:', data);
-    });
-  }
-
-  getUsers() {
-    this.users$ = this.credits.getUsers();
-  }
-
-  creditsTransaction(credits: number | string, id: number | string) {
-    // .subscribe() reacts to the data's arrival
-    this.credits.creditsTransaction(credits, id).subscribe((response) => {
-      this.getUsers();
-    });
-
-    console.log('transaction test');
-  }
-}
+export class AppComponent {}
