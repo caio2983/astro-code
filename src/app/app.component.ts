e7933647506d7f6fa3133f220f9d7d8b838a21e3
caio2983 <caioluna2002@gmail.com>
2025-04-18 22:46:46 -0300

    update misc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { CreditsService } from './credits.service';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { AsyncPipe, NgFor } from '@angular/common';
 import { UserlistComponent } from './userlist/userlist.component';
+import { PaymentComponent } from './payment/payment.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 @Component({
   selector: 'app-root',
-  imports: [UserlistComponent],
+  imports: [UserlistComponent, PaymentComponent, TransactionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
